fa9b3e71ce8cddff2226f03d49cee470a80a4da8
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-08-13 14:57:01 +0500

    Attribute binding

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>{{ message }}</h1>
     <p v-html="tag" ></p>
+    <p v-bind:class="box"> this is Attribute binding</p>
   </div>
 </template>
 
@@ -10,4 +11,13 @@ import { ref } from 'vue';
 
 const message = ref('Hello, Vue 3!');
 const tag=ref('<h1>Interpolation Example</h1>');
+const box="container";
 </script>
+
+<style scoped>
+.container {
+  background-color: rgb(25, 112, 141);
+  padding: 20px;
+  border-radius: 5px;
+}
+</style>
