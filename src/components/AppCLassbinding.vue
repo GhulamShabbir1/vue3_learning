bad7f80413cc03c9977edd2ac32b0e930ff2aa26
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-08-18 17:34:22 +0500

    updates

---
+++ src/components/AppCLassbinding.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <h1>App Class Binding Example</h1>
+        <p>This component demonstrates how to use class binding in Vue.js.</p>
+
+        <div class="contain" :class="{ active: isActive, 'text-large': isLarge }">
+            This text will change class based on the component's data properties.
+        </div>
+       
+        <div >
+            <p class="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quia quasi hic atque ab! Autem a velit cupiditate voluptatibus dolorem.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue';
+export default {
+    name: 'AppClassbinding',
+    setup() {
+        const isActive = ref(false);
+        const isLarge = ref(false);
+        const isvisible = ref(false);
+
+        const container=reactive({
+            color: 'blue',
+            fontSize: '16px',
+            backgroundColor: 'red'
+        })
+
+        function toggleActive() {
+            isActive.value = !isActive.value;
+        }
+
+        function toggleSize() {
+            isLarge.value = !isLarge.value;
+        }
+
+        function isshow() {
+            isvisible.value = !isvisible.value;
+        }
+
+        return {
+            isActive,
+            isLarge,
+            toggleActive,
+            toggleSize,
+            isshow,
+            isvisible,
+            container
+        };
+    }
+};
+</script>
+
+<style scoped>
+.active {
+    color: green;
+}
+
+.text-large {
+    font-size: 20px;
+}
+
+.visible {
+    display: block;
+}
+
+.hide {
+    display: none;
+}
+
+.contain {
+    padding: 20px;
+    border: 1px solid #ccc;
+    margin-bottom: 20px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,19 +4,26 @@
     <p v-html="tag" ></p>
     <p v-bind:class="objectOfAttrs"> this is Attribute binding</p>
     <button :disabled="true"> Disable me</button>
+    <button @click="increment()">Increment</button>
+    <p>Count: {{ count }}</p>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-
+const count = ref(0);
 const message = ref('Hello, Vue 3!');
 const tag=ref('<h1>Interpolation Example</h1>');
+
 // const box="container";
 const objectOfAttrs = {
   id: 'container',
   class: 'wrapper',
   style: 'background-color:green'
+}
+
+function increment(count){
+  count.value++;
 }
 </script>
 
